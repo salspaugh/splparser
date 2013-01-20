e5a36bfc58c8e97114d0e9873b1732447b9f46e2
Sara Alspaugh <saraalspaugh@gmail.com>
2013-01-19 17:11:02 -0800

    Accidentally checked in the build and dist stuff. Removed them.

--- build/lib/splparser/cmdparsers/common/asrules.py
+++
@@ -1,9 +0,0 @@
-
-def p_as_lc(p):
-    """as : ASLC"""
-    p[0] = p[1]
-
-def p_as_uc(p):
-    """as : ASUC"""
-    p[0] = p[1]
-
--- build/lib/splparser/common/rules/uminusrules.py
+++
@@ -1,5 +0,0 @@
-
-def p_negint(p):
-    """int : MINUS int %prec UMINUS"""
-    p[0] = ParseTreeNode('INT', raw=''.join([p[1], p[2].raw]))
-
