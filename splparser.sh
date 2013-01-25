ce95950dd4b77cba4e77e96d94703fe5d1203362
Sara Alspaugh <saraalspaugh@gmail.com>
2013-01-24 21:07:10 -0800

    Now that there is no monolithic parser, the main lexer has been replaced by many individual lexers. This means the spllex function is obsolete, and has been deleted.

--- splparser.sh
+++ splparser.sh
@@ -4,8 +4,3 @@ function splparse {
     search=\'$1\' # bash will have stripped the quotes
     python -c "from splparser.parser import parse; parse($search, pdebug=True).print_tree()"    
 }
-
-function spllex {
-    arg=\'$1\'
-    python -c "from splparser.lexer import tokenize; print tokenize($arg)"
-}
