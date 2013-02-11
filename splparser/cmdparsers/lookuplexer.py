649270028f9857e35ae6fa6b6580d99a8ac6b5ff
Richie Zeng <richzeng@gmail.com>
2013-02-11 00:53:15 -0800

    more cleanup, deleted a lot more unnecessary lexer stuff and moved OUTPUT option to reservered keywords

--- splparser/cmdparsers/lookuplexer.py
+++ splparser/cmdparsers/lookuplexer.py
@@ -11,13 +11,14 @@ tokens = [
     'EQ',
     'IPV4ADDR', 'IPV6ADDR',
     'WORD',
-    'OUTPUT', 'OUTPUTNEW',
 ]
 
 reserved = {
     'lookup' : 'LOOKUP', 
     'as' : 'ASLC',
     'AS' : 'ASUC',
+    'OUTPUT' : 'OUTPUT',
+    'OUTPUTNEW' : 'OUTPUTNEW',
 }
 
 tokens = tokens + list(reserved.values())
@@ -29,63 +30,12 @@ t_EQ = r'='
 # !!!   The order in which these functions are defined determine matchine. The 
 #       first to match is used. Take CARE when reordering.
 
-
-def is_ipv4addr(addr):
-    addr = addr.replace('*', '0')
-    addr = addr.strip()
-    addr = addr.strip('"')
-    port = addr.find(':')
-    if port > 0:
-        addr = addr[:port]
-    slash = addr.find('/')
-    if slash > 0:
-        addr = addr[:slash]
-    addr = addr.strip()
-    import socket
-    try:
-        socket.inet_pton(socket.AF_INET, addr)
-    except socket.error:
-        return False
-    return True
-
-def is_ipv6addr(addr):
-    addr = addr.replace('*', '0')
-    addr = addr.strip()
-    addr = addr.strip('"')
-    addr = addr.strip('[')
-    port = addr.find(']')
-    if port > 0:
-        addr = addr[:port]
-    slash = addr.find('/')
-    if slash > 0:
-        addr = addr[:slash]
-    addr = addr.strip()
-    import socket
-    try:
-        socket.inet_pton(socket.AF_INET6, addr)
-    except socket.error:
-        return False
-    return True
-
 def type_if_reserved(t, default):
     return reserved.get(t.value, default)
-
-def t_OUTPUT(t):
-    r'OUTPUT'
-    return t
-
-def t_OUTPUTNEW(t):
-    r'OUTPUTNEW'
-    return t
 
 @TOKEN(word)
 def t_WORD(t):
     t.type = type_if_reserved(t, 'WORD')
-    return t
-
-@TOKEN(id)
-def t_ID(t):
-    t.type = type_if_reserved(t, 'ID')
     return t
 
 def t_error(t):
