02bbba20b746b40a37250dbf6599c4e3c04c04fe
Sara Alspaugh <saraalspaugh@gmail.com>
2013-02-12 14:57:02 -0800

    Cleaned up the table parser.

--- splparser/cmdparsers/tablelexer.py
+++ splparser/cmdparsers/tablelexer.py
@@ -4,16 +4,21 @@ import ply.lex
 from ply.lex import TOKEN
 import re
 
-from splparser.cmdparsers.fieldsregexes import *
+from splparser.cmdparsers.searchregexes import *
 from splparser.exceptions import SPLSyntaxError
 
 tokens = [
     'COMMA',
+    'MACRO',
     'WILDCARD',
+    'EQ', 'LT', 'LE', 'GE', 'GT', 'NE',
+    'DCOLON',
+    'LPAREN', 'RPAREN',
+    'IPV4ADDR', 'IPV6ADDR',
+    'EMAIL','HOSTNAME', 'URL', 'PATH', 'US_PHONE',
     'WORD',
     'INT', 'BIN', 'OCT', 'HEX', 'FLOAT',
     'ID',
-    'EMAIL',
     'NBSTR', # non-breaking string
     'LITERAL', # in quotes
 ]
@@ -25,18 +30,109 @@ reserved = {
 tokens = tokens + list(reserved.values())
 
 precedence = (
-    ('left', 'COMMA'),
+    ('nonassoc', 'NE' ,'LT', 'LE', 'GE', 'GT'),
+    ('left', 'OR'),
+    ('left', 'AND', 'COMMA'),
+    ('right', 'EQ', 'DCOLON'),
+    ('right', 'NOT'),
 )
 
 t_ignore = ' '
 
+t_EQ = r'='
+t_LT = r'<'
+t_LE = r'<='
+t_GE = r'>='
+t_GT = r'>'
+t_NE = r'!='
+t_DCOLON = r'::'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+
+# !!!   The order in which these functions are defined determine matchine. The 
+#       first to match is used. Take CARE when reordering.
+
+states = (
+    ('ipunchecked', 'inclusive'),
+)
+
+def is_ipv4addr(addr):
+    addr = addr.replace('*', '0')
+    addr = addr.strip()
+    addr = addr.strip('"')
+    port = addr.find(':')
+    if port > 0:
+        addr = addr[:port]
+    slash = addr.find('/')
+    if slash > 0:
+        addr = addr[:slash]
+    addr = addr.strip()
+    import socket
+    try:
+        socket.inet_pton(socket.AF_INET, addr)
+    except socket.error:
+        return False
+    return True
+
+def is_ipv6addr(addr):
+    addr = addr.replace('*', '0')
+    addr = addr.strip()
+    addr = addr.strip('"')
+    addr = addr.strip('[')
+    port = addr.find(']')
+    if port > 0:
+        addr = addr[:port]
+    slash = addr.find('/')
+    if slash > 0:
+        addr = addr[:slash]
+    addr = addr.strip()
+    import socket
+    try:
+        socket.inet_pton(socket.AF_INET6, addr)
+    except socket.error:
+        return False
+    return True
+
+def type_if_reserved(t, default):
+    if re.match(search_key, t.value):
+        return 'SEARCH_KEY'
+    else:
+        return reserved.get(t.value, default)
+
+def t_MACRO(t):
+    r"""(`[^`]*`)"""
+    return t
+
+@TOKEN(ipv4_addr)
+def t_ipunchecked_IPV4ADDR(t):
+    if is_ipv4addr(t.value):
+        return t
+    t.lexer.lexpos -= len(t.value)
+    t.lexer.begin('INITIAL')
+    return
+
+@TOKEN(ipv6_addr)
+def t_ipunchecked_IPV6ADDR(t):
+    if is_ipv6addr(t.value):
+        return t
+    t.lexer.lexpos -= len(t.value)
+    t.lexer.begin('INITIAL')
+    return
+
 def t_COMMA(t):
     r'''(?:\,)|(?:"\,")|(?:'\,')'''
+    t.lexer.begin('ipunchecked')
     return t
 
 @TOKEN(wildcard)
 def t_WILDCARD(t):
+    t.lexer.begin('ipunchecked')
     return t
+
+@TOKEN(search_key)
+def t_SEARCH_KEY(t):
+    t.lexer.begin('ipunchecked')
+    return(t)
 
 def t_LITERAL(t):
     r'"(?:[^"]+(?:(\s|-|_)+[^"]+)+\s*)"'
@@ -44,47 +140,80 @@ def t_LITERAL(t):
 
 @TOKEN(bin)
 def t_BIN(t):
+    t.lexer.begin('ipunchecked')
     return t
 
 @TOKEN(oct)
 def t_OCT(t):
+    t.lexer.begin('ipunchecked')
     return t
 
 @TOKEN(hex)
 def t_HEX(t):
+    t.lexer.begin('ipunchecked')
     return t
 
 @TOKEN(float)
 def t_FLOAT(t):
+    t.lexer.begin('ipunchecked')
     return t
 
 @TOKEN(word)
 def t_WORD(t):
-    t.type = reserved.get(t.value, 'WORD')
+    t.type = type_if_reserved(t, 'WORD')
+    t.lexer.begin('ipunchecked')
     return t
 
 @TOKEN(int)
 def t_INT(t):
+    t.lexer.begin('ipunchecked')
     return t
 
 @TOKEN(id)
 def t_ID(t):
-    t.type = reserved.get(t.value, 'ID')
+    t.type = type_if_reserved(t, 'ID')
+    t.lexer.begin('ipunchecked')
     return t
 
 @TOKEN(email)
 def t_EMAIL(t):
-    t.type = reserved.get(t.value, 'EMAIL')
+    t.type = type_if_reserved(t, 'EMAIL')
+    t.lexer.begin('ipunchecked')
     return t
+
+@TOKEN(hostname)
+def t_HOSTNAME(t):
+    t.type = type_if_reserved(t, 'HOSTNAME')
+    t.lexer.begin('ipunchecked')
+    return(t)
+
+@TOKEN(path)
+def t_PATH(t):
+    t.type = type_if_reserved(t, 'PATH')
+    t.lexer.begin('ipunchecked')
+    return(t)
+
+@TOKEN(url)
+def t_URL(t):
+    t.type = type_if_reserved(t, 'URL')
+    t.lexer.begin('ipunchecked')
+    return(t)
+
+@TOKEN(us_phone)
+def t_US_PHONE(t):
+    t.lexer.begin('ipunchecked')
+    return(t)
 
 @TOKEN(nbstr)
 def t_NBSTR(t): # non-breaking string
-    t.type = reserved.get(t.value, 'NBSTR')
+    t.type = type_if_reserved(t, 'NBSTR')
+    t.lexer.begin('ipunchecked')
     return t
 
 def t_error(t):
     badchar = t.value[0]
     t.lexer.skip(1)
+    t.lexer.begin('ipunchecked')
     raise SPLSyntaxError("Illegal character in search lexer '%s'" % badchar)
 
 lexer = ply.lex.lex()
@@ -92,6 +221,7 @@ lexer = ply.lex.lex()
 def tokenize(data, debug=False, debuglog=None):
     lexer = ply.lex.lex(debug=debug, debuglog=debuglog)
     lexer.input(data)
+    lexer.begin('ipunchecked')
     tokens = []
     while True:
         tok = lexer.token()
