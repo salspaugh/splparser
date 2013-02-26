8d1399d186ca74f7ff5a925c17f1e392c852e2bd
Sara Alspaugh <saraalspaugh@gmail.com>
2013-02-26 12:25:47 -0800

    Fixed a bug in the code that searched for the parsetab module.

--- splparser/parser.py
+++ splparser/parser.py
@@ -7,7 +7,7 @@ import ply.yacc
 
 class SPLParser(object):
 
-    def __init__(self, lexermod, parsetab_name, parsetab_dir, rulesmod):
+    def __init__(self, lexermod, parsetab_name, parsetab_dir, rulesmod, optimize=False):
         self.lexer = lexermod.lex()
         self.parsetab_name = parsetab_name
         self.parsetab_dir = parsetab_dir
@@ -17,24 +17,38 @@ class SPLParser(object):
         self.parser = ply.yacc.yacc(module=self.rules, 
                                     debuglog=self.log, 
                                     tabmodule=self.parsetab_name, 
-                                    outputdir=self.parsetab_dir)
+                                    outputdir=self.parsetab_dir,
+                                    optimize=optimize)
 
     def setup_parsetab(self):
-        here = os.path.dirname(__file__)
-        path_to_parsetab = os.path.join(here, self.parsetab_dir, self.parsetab_name + '.py')
-        try:
+        
+        loaded = False
+        try: # check for parsetabs in current installation
+            here = os.path.dirname(__file__)
+            path_to_parsetab = os.path.join(here, self.parsetab_dir, self.parsetab_name + '.py')
             parsetab = imp.load_source(self.parsetab_name, path_to_parsetab)
-        except IOError: # parsetab files don't exist in our installation
+            loaded = True
+        except IOError:
             parsetab = self.parsetab_name
-        try:
-            os.stat(self.parsetab_dir)
-        except:
-            try:
-                os.makedirs(self.parsetab_dir)
-            except OSError:
-                sys.stderr.write("ERROR: \
-                                  Need permission to write to ./" + self.parsetab_dir + "\n")
-                raise
+
+        if not loaded:
+            try: # check for parsetabs in current directory 
+                path_to_parsetab = os.path.join(self.parsetab_dir, self.parsetab_name + '.py')
+                parsetab = imp.load_source(self.parsetab_name, path_to_parsetab)
+            except IOError:
+                parsetab = self.parsetab_name
+
+        if not loaded:
+            try: # in case the above failed, create dir for PLY to write parsetabs in
+                os.stat(self.parsetab_dir)
+            except:
+                try:
+                    os.makedirs(self.parsetab_dir)
+                except OSError:
+                    sys.stderr.write("ERROR: \
+                                      Need permission to write to ./" + self.parsetab_dir + "\n")
+                    raise
+
         return parsetab
     
     def setup_log(self):
