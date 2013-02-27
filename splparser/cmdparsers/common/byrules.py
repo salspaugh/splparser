000be50c6cedefcc503dbdf3d587e794f6baf57e
Sara Alspaugh <saraalspaugh@gmail.com>
2013-02-26 19:33:09 -0800

    Deleting unnecessary package now.

--- splparser/cmdparsers/common/byrules.py
+++
@@ -1,8 +0,0 @@
-
-def p_by_lc(p):
-    """by : BYLC"""
-    p[0] = p[1]
-
-def p_by_uc(p):
-    """by : BYUC"""
-    p[0] = p[1]
--- splparser/cmdparsers/common/uminusrules.py
+++
@@ -1,7 +0,0 @@
-
-from splparser.parsetree import *
-
-def p_negint(p):
-    """int : MINUS int %prec UMINUS"""
-    p[0] = ParseTreeNode('INT', raw=''.join([p[1], p[2].raw]))
-
