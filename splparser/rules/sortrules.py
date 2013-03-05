0af6bb41560dbfc1c7543d49a1973e36d834f93f
Richie Zeng <richzeng@gmail.com>
2013-03-04 20:06:21 -0800

    removed extraneous things in the sortrules

--- splparser/rules/sortrules.py
+++ splparser/rules/sortrules.py
@@ -24,10 +24,9 @@ def p_sort_sortfields(p):
     p[0].add_children(p[2])
 
 def p_sort_num_sortfields(p):
-    """sortcmd : SORT INT sortfields"""
+    """sortcmd : SORT value sortfields"""
     p[0] = ParseTreeNode('SORT')
-    num = ParseTreeNode('INT', raw=p[2])
-    p[0].add_children([num] + p[3])
+    p[0].add_children([p[2]] + p[3])
 
 def p_sort_num_limit_sortfields(p):
     """sortcmd : SORT field EQ value sortfields"""
@@ -64,25 +63,5 @@ def p_sortfunc(p):
     p[0] = ParseTreeNode(p[1].upper())
     p[0].add_child(p[3])
 
-
 def p_error(p):
     raise SPLSyntaxError("Syntax error in sort parser input!")
-
-logging.basicConfig(
-    level = logging.DEBUG,
-    filename = "sortupparser.log",
-    filemode = "w",
-    format = "%(filename)10s:%(lineno)4d:%(message)s"
-)
-
-log = logging.getLogger()
-
-def parse(data, ldebug=False, ldebuglog=log, pdebug=False, pdebuglog=log):
-    parser = ply.yacc.yacc(debug=pdebug, debuglog=pdebuglog, tabmodule="sort_parsetab")
-    return parser.parse(data, debug=pdebuglog, lexer=lexer)
-
-if __name__ == "__main__":
-    import sys
-    lexer = ply.lex.lex()
-    parser = ply.yacc.yacc()
-    print parser.parse(sys.argv[1:], debug=log, lexer=lexer)
