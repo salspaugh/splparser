abd3b41bfb9b76ca47afe664e02d037073527c91
Richie Zeng <richzeng@gmail.com>
2013-03-19 13:48:55 -0700

    almost dont wieh inputlookup. filenames are getting parsed as hostnames thought...

---
+++ splparser/rules/inputlookuprules.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from splparser.parsetree import *
+from splparser.exceptions import SPLSyntaxError
+
+from splparser.rules.common.fieldrules import *
+
+from splparser.lexers.inputlookuplexer import tokens
+
+start = 'cmdexpr'
+
+def p_cmdexpr_inputlookup(p):
+    """cmdexpr : inputlookupcmd"""
+    p[0] = p[1]
+
+def p_inputlookup_name(p):
+    """inputlookupcmd : INPUTLOOKUP field"""
+    p[0] = ParseTreeNode('INPUTLOOKUP')
+    p[0].add_child(p[2])
+
+def p_inputlookup_optionlist_name(p):
+    """inputlookupcmd : INPUTLOOKUP optionlist field"""
+    p[0] = ParseTreeNode('INPUTLOOKUP')
+    p[0].add_children(p[2] + [p[3]])
+
+def p_optionlist_single(p):
+    """optionlist : INPUTLOOKUP_OPT EQ field"""
+    eq = ParseTreeNode('EQ')
+    eq.add_children([ParseTreeNode('WORD', raw=p[1]), p[3]])
+    p[0] = [eq]
+
+def p_optionlist(p):
+    """optionlist : INPUTLOOKUP_OPT EQ field optionlist"""
+    eq = ParseTreeNode('EQ')
+    eq.add_children([ParseTreeNode('WORD', raw=p[1]), p[3]])
+    p[0] = [eq] + p[4]
+
+def p_error(p):
+    raise SPLSyntaxError("Syntax error in inputlookup parser input!")
