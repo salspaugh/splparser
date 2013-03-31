46ec92d1837dfb3ac3f99c293ea03a44fbf2a758
Sara Alspaugh <saraalspaugh@gmail.com>
2013-03-31 02:03:17 -0700

    Added relevancy command.

---
+++ splparser/rules/relevancyrules.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from splparser.parsetree import *
+from splparser.exceptions import SPLSyntaxError
+
+from splparser.lexers.relevancylexer import tokens
+
+start = 'cmdexpr'
+
+def p_cmdexpr_relevancy(p):
+    """cmdexpr : relevancycmd"""
+    p[0] = p[1]
+
+def p_cmdexpr_relevancy_debug(p):
+    """relevancycmd : RELEVANCY"""
+    p[0] = ParseTreeNode('RELEVANCY')
+
+def p_error(p):
+    raise SPLSyntaxError("Syntax error in relevancy parser input!") 
