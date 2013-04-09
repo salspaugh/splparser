31c4de1177c216ec1e2e648d20bec5d6a8e86d73
keroro824 <bettychen824@gmail.com>
2013-04-08 23:04:14 -0700

    change the name of the function

--- splparser/rules/makemvrules.py
+++ splparser/rules/makemvrules.py
@@ -13,45 +13,45 @@ from splparser.lexers.makemvlexer import tokens
 
 start = 'cmdexpr'
 
-def p_cmdexpr_addtotals(p):
+def p_cmdexpr_makemv(p):
     """cmdexpr : makemvcmd"""
     p[0] = p[1]
 
-def p_cmdexpr_addtotals_single(p):
+def p_cmdexpr_makemv_single(p):
     """makemvcmd : MAKEMV"""
     p[0] = ParseTreeNode('MAKEMV')
 
-def p_cmdexpr_addtotals_field(p):
+def p_cmdexpr_makemv_field(p):
     """makemvcmd : MAKEMV wc_stringlist field"""
     p[0] = ParseTreeNode('MAKEMV')
     p[0].add_children(p[2].children)
     p[0].add_child(p[3])
 
-def p_addtotalscmd_addtotals(p):
+def p_addtotalscmd_makemv(p):
     """makemvcmd : MAKEMV wc_stringlist
                  | MAKEMV field"""
     p[0] = ParseTreeNode('MAKEMV')
     p[0].add_child(p[2])
 
-def p_addtotals_opt_comma(p):
+def p_makemv_opt_comma(p):
     """wc_string : MAKEMV_OPT EQ COMMA"""
     p[0] = ParseTreeNode('EQ')
     p[1] = ParseTreeNode(p[1].upper())
     p[3] = ParseTreeNode(p[3])
     p[0].add_children([p[1],p[3]])
 
-def p_addtotals_opt(p):
+def p_makemv_opt(p):
     """wc_string : MAKEMV_OPT EQ value"""
     p[0] = ParseTreeNode('EQ')
     p[1] = ParseTreeNode(p[1].upper())
     p[0].add_children([p[1],p[3]])
 
-def p_addtotals_opt_list(p):
+def p_makemv_opt_list(p):
     """wc_stringlist : wc_string"""
     p[0] = ParseTreeNode('EQ')
     p[0].add_child(p[1])
 
-def p_addtotals_optlist(p):
+def p_makemv_optlist(p):
     """wc_stringlist : wc_string wc_stringlist"""
     p[0] = ParseTreeNode('EQ')
     p[0].add_child(p[1])
