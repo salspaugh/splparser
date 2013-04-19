1a775a94232359f4651ccd7ea9fc8e79e3f0339e
Sara Alspaugh <saraalspaugh@gmail.com>
2013-04-19 14:10:25 -0700

    Changed more parser rules for schema detection.

--- splparser/rules/rarerules.py
+++ splparser/rules/rarerules.py
@@ -17,13 +17,13 @@ def p_cmdexpr_rare(p):
 
 def p_rare_fieldlist(p):
     """rarecmd : RARE fieldlist
-              | SIRARE fieldlist"""
+               | SIRARE fieldlist"""
     p[0] = ParseTreeNode(p[1].upper())
     p[0].add_children(p[2].children)
 
 def p_rare_fieldlist_by(p):
     """rarecmd : RARE fieldlist by fieldlist
-              | SIRARE fieldlist by fieldlist"""
+               | SIRARE fieldlist by fieldlist"""
     by_node = ParseTreeNode('BY')
     by_node.add_children(p[4].children)
     p[0] = ParseTreeNode(p[1].upper())
@@ -32,7 +32,7 @@ def p_rare_fieldlist_by(p):
 
 def p_rare_rareopt_fieldlist(p):
     """rarecmd : RARE rareoptlist fieldlist
-              | SIRARE rareoptlist fieldlist"""
+               | SIRARE rareoptlist fieldlist"""
     p[0] = ParseTreeNode(p[1].upper())
     p[0].add_children(p[2].children)
     p[0].add_children(p[3].children)
@@ -41,15 +41,17 @@ def p_rare_weird(p):
     """rarecmd : RARE INT fieldlist
                | SIRARE INT fieldlist"""
     p[0] = ParseTreeNode(p[1].upper())
-    Node = ParseTreeNode('LIMIT')
-    Node_2 = ParseTreeNode('INT', raw=p[2], arg=True)
-    Node.add_child(Node_2)
-    p[0].add_child(Node)
+    eq_node = ParseTreeNode('EQ')
+    p[0].add_child(eq_node)
     p[0].add_children(p[3].children)
+    limit_node = ParseTreeNode('LIMIT', option=True)
+    int_node = ParseTreeNode('INT', raw=p[2], arg=True)
+    eq_node.add_children([limit_node, int_node])
+    limit_node.values.append(int_node)
 
 def p_rare_rareopt_fieldlist_by(p):
     """rarecmd : RARE rareoptlist fieldlist by fieldlist
-              | SIRARE rareoptlist fieldlist by fieldlist"""
+               | SIRARE rareoptlist fieldlist by fieldlist"""
     by_node = ParseTreeNode('BY')
     by_node.add_children(p[5].children)
     p[0] = ParseTreeNode(p[1].upper())
@@ -70,12 +72,18 @@ def p_rareoptlist_rareopt(p):
 
 def p_rareopt(p):
     """rareopt : TOP_OPT EQ value"""
-    p[0] = ParseTreeNode(p[1].upper())
+    p[0] = ParseTreeNode('EQ')
+    opt_node = ParseTreeNode(p[1].upper())
+    opt_node.values.append(p[3])
+    p[0].add_child(opt_node)
     p[0].add_child(p[3])
 
 def p_rareopt_commonopt(p):
     """rareopt : COMMON_OPT EQ value"""
-    p[0] = ParseTreeNode(p[1].upper())
+    p[0] = ParseTreeNode('EQ')
+    opt_node = ParseTreeNode(p[1].upper())
+    opt_node.values.append(p[3])
+    p[0].add_child(opt_node)
     p[0].add_child(p[3])
 
 def p_error(p):
