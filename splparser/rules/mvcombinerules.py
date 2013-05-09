78696478bd6fecb8803ad8a1c98efcc6bb358736
Beidi Chen <bettychen824@gmail.com>
2013-05-08 23:32:28 -0700

    fix lexer and add option value

--- splparser/rules/mvcombinerules.py
+++ splparser/rules/mvcombinerules.py
@@ -32,7 +32,8 @@ def p_fieldlist(p):
 def p_value(p):
     """m_delim : DELIM EQ value"""
     p[0] = ParseTreeNode('EQ')
-    p[1] = ParseTreeNode(p[1].upper())
+    p[1] = ParseTreeNode(p[1].upper(), option=True)
+    p[1].values.append(p[3])
     p[0].add_children([p[1],p[3]])
 
 def p_comma(p):
