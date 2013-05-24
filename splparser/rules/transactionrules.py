7eadf6f4f1a2e4d13a8a531715ccc2fcd72f7f87
Richie Zeng <richzeng@gmail.com>
2013-05-24 16:21:05 -0700

    updated transaction tests

--- splparser/rules/transactionrules.py
+++ splparser/rules/transactionrules.py
@@ -22,21 +22,21 @@ def p_transaction_options(p):
 def p_transaction_fieldlist_options(p):
     """transactioncmd : TRANSACTION fieldlist optionlist"""
     p[0] = ParseTreeNode('TRANSACTION')
-    p[0].add_children([p[2]] + p[3])
+    p[0].add_children(p[2].children + p[3])
 
 def p_optionlist_single(p):
     """optionlist : TRANSACTION_OPT EQ field"""
+    eq = ParseTreeNode('EQ')    
     opt = ParseTreeNode(p[1].upper(), option=True)
-    opt.values.append(p[3])
-    opt.add_child(p[3])
-    p[0] = [opt]
+    eq.add_children([opt, p[3]])
+    p[0] = [eq]
 
 def p_optionlist(p):
     """optionlist : TRANSACTION_OPT EQ field optionlist"""
-    opt= ParseTreeNode(p[1].upper(), option=True)
-    opt.values.append(p[3])
-    opt.add_child(p[3])
-    p[0] = [opt] + p[4]
+    eq = ParseTreeNode('EQ')    
+    opt = ParseTreeNode(p[1].upper(), option=True)
+    eq.add_children([opt, p[3]])
+    p[0] = [eq] + p[4]
 
 
 def p_error(p):
