32e1a2601fa0128bdaa1520da3e29acf3477fa3b
Sara Alspaugh <saraalspaugh@gmail.com>
2013-07-15 15:09:06 -0700

    Sort of fixed the head command.

--- splparser/rules/headrules.py
+++ splparser/rules/headrules.py
@@ -20,32 +20,40 @@ def p_headcmd_head(p):
     p[0] = ParseTreeNode('HEAD')
 
 def p_headcmd_head_int(p):
-    """headcmd : HEAD int"""
-    p[0] = ParseTreeNode('HEAD')
+    """headcmd : HEAD int""" # FIXME: reduce/reduce error
     p[2].role = 'VALUE'
-    p[0].add_child(p[2])
+    p[0] = ParseTreeNode('COMMAND', raw='head')
+    eq_node = ParseTreeNode('EQ')
+    p[0].add_child(eq_node)
+    opt_node = ParseTreeNode('OPTION', raw='limit')
+    opt_node.values.append(p[2])
+    eq_node.add_children([opt_node, p[2]])
 
 def p_headcmd_head_eval(p):
-    """headcmd : HEAD evalfnexpr"""
-    p[0] = ParseTreeNode('HEAD')
-    p[0].add_child(p[2])
+    """headcmd : HEAD oplist"""
+    p[0] = ParseTreeNode('COMMAND', raw='head')
+    p[0].add_children(p[2].children)
 
 def p_headcmd_head_headopt(p):
     """headcmd : HEAD headoptlist"""
-    p[0] = ParseTreeNode('HEAD') 
+    p[0] = ParseTreeNode('COMMAND', raw='head') 
     p[0].add_children(p[2].children)
 
 def p_headcmd_head_int_headopt(p):
     """headcmd : HEAD int headoptlist"""
-    p[0] = ParseTreeNode('HEAD')
     p[2].role = 'VALUE'
-    p[0].add_child(p[2])
+    p[0] = ParseTreeNode('COMMAND', raw='head')
+    eq_node = ParseTreeNode('EQ')
+    p[0].append(eq_node)
+    opt_node = ParseTreeNode('OPTION', raw='limit')
+    eq_node.add_children([opt_node, p[2]])
+    opt_node.values.append(p[2])
     p[0].add_children(p[3].children)
 
 def p_headcmd_head_eval_headopt(p):
-    """headcmd : HEAD evalfnexpr headoptlist"""
-    p[0] = ParseTreeNode('HEAD')
-    p[0].add_child(p[2])
+    """headcmd : HEAD oplist headoptlist"""
+    p[0] = ParseTreeNode('COMMAND', raw='head')
+    p[0].add_children(p[2].children)
     p[0].add_children(p[3].children)
 
 def p_headoptlist(p):
@@ -61,15 +69,17 @@ def p_headoptlist_headopt(p):
 
 def p_headopt(p):
     """headopt : HEAD_OPT EQ simplevalue"""
-    p[0] = ParseTreeNode('OPTION', raw=p[1])
-    p[0].values.append(p[3])
-    p[0].add_child(p[3])
+    p[0] = ParseTreeNode('EQ')
+    opt_node = ParseTreeNode('OPTION', raw=p[1])
+    p[0].add_children([opt_node, p[3]])
+    opt_node.values.append(p[3])
 
 def p_headopt_commonopt(p):
     """headopt : COMMON_OPT EQ simplevalue"""
-    p[0] = ParseTreeNode('OPTION', raw=p[1])
-    p[0].values.append(p[3])
-    p[0].add_child(p[3])
+    p[0] = ParseTreeNode('EQ')
+    opt_node = ParseTreeNode('OPTION', raw=p[1])
+    p[0].add_children([opt_node, p[3]])
+    opt_node.values.append(p[3])
 
 def p_error(p):
     raise SPLSyntaxError("Syntax error in head parser input!") 
