1fd8bf35fd9436e1577d9d661e64c29393e8e292
Sara Alspaugh <saraalspaugh@gmail.com>
2013-07-19 16:16:08 -0700

    Updated xpath command.

--- splparser/rules/xpathrules.py
+++ splparser/rules/xpathrules.py
@@ -13,38 +13,42 @@ from splparser.lexers.xpathlexer import tokens
 
 start = 'cmdexpr'
 
-def p_cmdexpr_xpath(p):
+def p_cmdexpr(p):
     """cmdexpr : xpathcmd"""
     p[0] = p[1]
 
-def p_cmdexpr_xpath_single(p):
-    """xpathcmd : XPATH"""
-    p[0] = ParseTreeNode('XPATH')
+def p_cmdexpr_xpath(p):
+    """xpathcmd : XPATH value"""
+    p[0] = ParseTreeNode('COMMAND', raw='xpath')
+    p[0].add_child(p[2])
 
-def p_cmdexpr_xpath_field(p):
-    """xpathcmd : XPATH wc_stringlist field"""
-    p[0] = ParseTreeNode('XPATH')
+def p_cmdexpr_xpath_opt_value_opt(p):
+    """xpathcmd : XPATH wc_stringlist value wc_stringlist"""
+    p[0] = ParseTreeNode('COMMAND', raw='xpath')
+    p[0].add_children(p[2].children)
+    p[0].add_child(p[3])
+    p[0].add_children(p[4].children)
+
+def p_addtotalscmd_opt_value(p):
+    """xpathcmd : XPATH wc_stringlist value"""
+    p[0] = ParseTreeNode('COMMAND', raw='xpath')
     p[0].add_children(p[2].children)
     p[0].add_child(p[3])
 
-def p_cmdexpr_xpath_field_list(p):
-    """xpathcmd : XPATH wc_stringlist field wc_stringlist"""
-    p[0] = ParseTreeNode('XPATH')
-    p[0].add_children(p[2].children)
-    p[0].add_child([p[3],p[4]])
-
-def p_addtotalscmd_xpath(p):
-    """xpathcmd : XPATH wc_stringlist
-                | XPATH field"""
-    p[0] = ParseTreeNode('XPATH')
+def p_addtotalscmd_value_opt(p):
+    """xpathcmd : XPATH value wc_stringlist"""
+    p[0] = ParseTreeNode('COMMAND', raw='xpath')
     p[0].add_child(p[2])
+    p[0].add_children(p[3].children)
 
 def p_xpath_opt(p):
     """wc_string : XPATH_OPT EQ value"""
     p[0] = ParseTreeNode('EQ', raw='assign')
-    p[1] = ParseTreeNode(p[1].upper(), option=True)
+    p[1] = ParseTreeNode('OPTION', raw=p[1])
+    if p[1].raw in ["field", "outfield"]:
+        p[3].role = 'FIELD'
     p[1].values.append(p[3])
-    p[0].add_children([p[1],p[3]])
+    p[0].add_children([p[1], p[3]])
 
 def p_xpath_opt_list(p):
     """wc_stringlist : wc_string"""
