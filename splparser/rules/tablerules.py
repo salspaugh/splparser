fc255371c1fce0b7a1c4716d91260a8ecaa54cc9
Sara Alspaugh <saraalspaugh@gmail.com>
2013-07-20 18:46:31 -0700

    Updated table rule.

--- splparser/rules/tablerules.py
+++ splparser/rules/tablerules.py
@@ -16,11 +16,11 @@ def p_cmdexpr_table(p):
 
 def p_cmdexpr_table_debug(p):
     """tablecmd : TABLE"""
-    p[0] = ParseTreeNode('TABLE')
+    p[0] = ParseTreeNode('COMMAND', raw='table')
 
 def p_table_fieldlist(p):
     """tablecmd : TABLE fieldlist"""
-    p[0] = ParseTreeNode('TABLE')
+    p[0] = ParseTreeNode('COMMAND', raw='table')
     p[0].add_children(p[2].children)
 
 def p_error(p):
