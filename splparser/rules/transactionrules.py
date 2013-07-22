28e2a447f7eb5a4c92ce334352ad0c122d084d00
Sara Alspaugh <saraalspaugh@gmail.com>
2013-07-22 12:01:01 -0700

    Updated transaction command.

--- splparser/rules/transactionrules.py
+++ splparser/rules/transactionrules.py
@@ -10,24 +10,29 @@ from splparser.lexers.transactionlexer import tokens
 
 start = 'cmdexpr'
 
+boolean_options = ["connected", "keeporphans", "unifyends", "keepevicted", "mvlist", "mvraw"]
+
 def p_cmdexpr_transaction(p):
     """cmdexpr : transactioncmd"""
     p[0] = p[1]
 
 def p_transaction_options(p):
     """transactioncmd : TRANSACTION optionlist"""
-    p[0] = ParseTreeNode('TRANSACTION')
+    p[0] = ParseTreeNode('COMMAND', raw='transaction')
     p[0].add_children(p[2])
 
 def p_transaction_fieldlist_options(p):
     """transactioncmd : TRANSACTION fieldlist optionlist"""
-    p[0] = ParseTreeNode('TRANSACTION')
+    p[0] = ParseTreeNode('COMMAND', raw='transaction')
     p[0].add_children(p[2].children + p[3])
 
 def p_optionlist_single(p):
     """optionlist : TRANSACTION_OPT EQ field"""
     eq = ParseTreeNode('EQ', raw='assign')    
     opt = ParseTreeNode('OPTION', raw=p[1])
+    p[3].role = 'VALUE'
+    if opt.raw in boolean_options:
+        p[3].type = 'BOOLEAN'
     eq.add_children([opt, p[3]])
     p[0] = [eq]
 
@@ -35,6 +40,9 @@ def p_optionlist(p):
     """optionlist : TRANSACTION_OPT EQ field optionlist"""
     eq = ParseTreeNode('EQ', raw='assign')    
     opt = ParseTreeNode('OPTION', raw=p[1])
+    p[3].role = 'VALUE'
+    if opt.raw in boolean_options:
+        p[3].type = 'BOOLEAN'
     eq.add_children([opt, p[3]])
     p[0] = [eq] + p[4]
 
