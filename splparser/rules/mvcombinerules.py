e47742fd0479ee757bbaed33143d2ecc655b133e
Sara Alspaugh <saraalspaugh@gmail.com>
2013-07-21 21:05:26 -0700

    Updated mvcombine rules.

--- splparser/rules/mvcombinerules.py
+++ splparser/rules/mvcombinerules.py
@@ -15,33 +15,29 @@ def p_cmdexpr_fillnull(p):
     """cmdexpr : mvcombinecmd"""
     p[0] = p[1]
 
-def p_fillnullcmd_fillnull(p):
-    """mvcombinecmd : MVCOMBINE"""
-    p[0] = ParseTreeNode('MVCOMBINE')
-
 def p_fillnullcmd_value(p):
     """mvcombinecmd : MVCOMBINE field"""
-    p[0] = ParseTreeNode('MVCOMBINE')
+    p[0] = ParseTreeNode('COMMAND', raw='mvcombine')
     p[0].add_child(p[2])
 
 def p_fieldlist(p):
     """mvcombinecmd : MVCOMBINE m_delim field"""
-    p[0] = ParseTreeNode('MVCOMBINE')
-    p[0].add_children([p[2],p[3]])
+    p[0] = ParseTreeNode('COMMAND', raw='mvcombine')
+    p[0].add_children([p[2], p[3]])
 
 def p_value(p):
     """m_delim : DELIM EQ value"""
     p[0] = ParseTreeNode('EQ', raw='assign')
     p[1] = ParseTreeNode('OPTION', raw=p[1])
     p[1].values.append(p[3])
-    p[0].add_children([p[1],p[3]])
+    p[0].add_children([p[1], p[3]])
 
 def p_comma(p):
     """m_delim : DELIM EQ COMMA"""
     p[0] = ParseTreeNode('EQ', raw='assign')
-    p[1] = ParseTreeNode(p[1].upper())
-    p[3] = ParseTreeNode(p[3])
-    p[0].add_children([p[1],p[3]])
+    p[1] = ParseTreeNode('OPTION', raw=p[1])
+    p[3] = ParseTreeNode('VALUE', type='LITERAL', raw=p[3])
+    p[0].add_children([p[1], p[3]])
 
 def p_error(p):
     raise SPLSyntaxError("Syntax error in mvcombine parser input!") 
