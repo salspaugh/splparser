19890073e2205fb5dceef8fd35407396f5f53ed4
Sara Alspaugh <saraalspaugh@gmail.com>
2013-07-22 14:09:55 -0700

    Update delta command.

--- splparser/rules/deltarules.py
+++ splparser/rules/deltarules.py
@@ -15,28 +15,25 @@ def p_cmdexpr_delta(p):
 
 def p_delta_arg(p):
     """deltacmd : DELTA deltaarg"""
-    p[0] = ParseTreeNode('DELTA')
+    p[0] = ParseTreeNode('COMMAND', raw='delta')
     p[0].add_child(p[2])
 
 def p_delta_arg_opt(p):
     """deltacmd : DELTA deltaarg deltaopt"""
-    p[0] = ParseTreeNode('DELTA')
+    p[0] = ParseTreeNode('COMMAND', raw='delta')
     p[0].add_child(p[2])
     p[0].add_child(p[3])
 
 def p_delta_opt_arg(p):
     """deltacmd : DELTA deltaopt deltaarg"""
-    p[0] = ParseTreeNode('DELTA')
+    p[0] = ParseTreeNode('COMMAND', raw='delta')
     p[0].add_child(p[2])
     p[0].add_child(p[3])
 
 def p_deltaarg_as(p):
     """deltaarg : field as field"""
-    p[0] = p[1]
-    as_node = ParseTreeNode('AS')
-    p[0].add_child(as_node)
-    as_node.add_child(p[3])
-    p[1].renamed = p[3]
+    p[0] = ParseTreeNode('FUNCTION', raw='as')
+    p[0].add_children([p[1], p[3]])
 
 def p_deltaarg(p):
     """deltaarg : field"""
@@ -48,7 +45,7 @@ def p_deltaopt(p):
     opt_node = ParseTreeNode('OPTION', raw=p[1])
     opt_node.values.append(p[3])
     p[0].add_child(opt_node)
-    int_node = ParseTreeNode('INT', raw=p[3], arg=True)
+    int_node = ParseTreeNode('VALUE', type='INT', raw=p[3], is_argument=True)
     p[0].add_child(int_node)
 
 def p_error(p):
