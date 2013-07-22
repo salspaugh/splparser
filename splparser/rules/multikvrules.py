ef79bbc4c59112534b495a8ebd9c5e608e94f8dd
Sara Alspaugh <saraalspaugh@gmail.com>
2013-07-21 20:21:42 -0700

    Updated multikv command.

--- splparser/rules/multikvrules.py
+++ splparser/rules/multikvrules.py
@@ -10,23 +10,25 @@ from splparser.lexers.multikvlexer import tokens
 
 start = 'cmdexpr'
 
+boolean_options = ["copyattrs", "noheader", "rmorig", "multitable"]
+
 def p_cmdexpr_multikvs(p):
     """cmdexpr : multikvcmd"""
     p[0] = p[1]
 
 def p_multikvcmd_multikv(p):
     """multikvcmd : MULTIKV"""
-    p[0] = ParseTreeNode('MULTIKV')
+    p[0] = ParseTreeNode('COMMAND', raw='multikv')
     p[0].error = True
 
 def p_multikv_multikvopt_fieldlist(p):
     """multikvcmd : MULTIKV multikvoptlist"""
-    p[0] = ParseTreeNode('MULTIKV')
+    p[0] = ParseTreeNode('COMMAND', raw='multikv')
     p[0].add_children(p[2].children)
 
 def p_conf_multikvoptlist(p):
     """multikvcmd : MULTIKV conf multikvoptlist"""
-    p[0] = ParseTreeNode('MULTIKV')
+    p[0] = ParseTreeNode('COMMAND', raw='multikv')
     p[0].add_child(p[2])
     p[0].add_children(p[3].children)
     
@@ -53,6 +55,8 @@ def p_multikvopt(p):
     """multikvopt : MULTIKV_SINGLE_OPT EQ value"""
     p[0] = ParseTreeNode('EQ', raw='assign')
     opt_node = ParseTreeNode('OPTION', raw=p[1])
+    if opt_node.raw in boolean_options:
+        p[3].type = 'BOOLEAN'
     opt_node.values.append(p[3])
     p[0].add_child(opt_node)
     p[0].add_child(p[3])
