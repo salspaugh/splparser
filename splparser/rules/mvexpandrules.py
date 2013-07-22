e6b9a7d6181f57eea07304dae60537a13c644e43
Sara Alspaugh <saraalspaugh@gmail.com>
2013-07-21 21:16:31 -0700

    Updated mvexpand command.

--- splparser/rules/mvexpandrules.py
+++ splparser/rules/mvexpandrules.py
@@ -11,30 +11,31 @@ from splparser.exceptions import SPLSyntaxError
 
 start = 'cmdexpr'
 
-def p_cmdexpr_fillnull(p):
+def p_cmdexpr_mvexpand(p):
     """cmdexpr : mvexpandcmd"""
     p[0] = p[1]
 
-def p_fillnullcmd_fillnull(p):
-    """mvexpandcmd : MVEXPAND"""
-    p[0] = ParseTreeNode('MVEXPAND')
-
-def p_fillnullcmd_value(p):
+def p_mvexpandcmd_field(p):
     """mvexpandcmd : MVEXPAND field"""
-    p[0] = ParseTreeNode('MVEXPAND')
+    p[0] = ParseTreeNode('COMMAND', raw='mvexpand')
     p[0].add_child(p[2])
 
-def p_fieldlist(p):
+def p_mvexpandcmd_field_limit(p):
     """mvexpandcmd : MVEXPAND field m_limit"""
-    p[0] = ParseTreeNode('MVEXPAND')
-    p[0].add_children([p[2],p[3]])
+    p[0] = ParseTreeNode('COMMAND', raw='mvexpand')
+    p[0].add_children([p[2], p[3]])
+
+def p_mvexpandcmd_limit_field(p):
+    """mvexpandcmd : MVEXPAND m_limit field"""
+    p[0] = ParseTreeNode('COMMAND', raw='mvexpand')
+    p[0].add_children([p[2], p[3]])
 
 def p_value(p):
     """m_limit : LIMIT EQ value"""
     p[0] = ParseTreeNode('EQ', raw='assign')
     p[1] = ParseTreeNode('OPTION', raw=p[1])
     p[1].values.append(p[3])
-    p[0].add_children([p[1],p[3]])
+    p[0].add_children([p[1], p[3]])
 
 def p_error(p):
     raise SPLSyntaxError("Syntax error in mvexpand parser input!") 
