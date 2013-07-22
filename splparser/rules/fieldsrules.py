654ba868a7d3d9fdda95da493e804323c1009cf8
Sara Alspaugh <saraalspaugh@gmail.com>
2013-07-21 21:36:02 -0700

    Updated fields command.

--- splparser/rules/fieldsrules.py
+++ splparser/rules/fieldsrules.py
@@ -14,27 +14,23 @@ def p_cmdexpr_fields(p):
     """cmdexpr : fieldscmd"""
     p[0] = p[1]
 
-def p_cmdexpr_fields_debug(p):
-    """fieldscmd : FIELDS"""
-    p[0] = ParseTreeNode('FIELDS')
-
 def p_fields_fieldlist(p):
     """fieldscmd : FIELDS fieldlist"""
-    p[0] = ParseTreeNode('FIELDS')
+    p[0] = ParseTreeNode('COMMAND', raw='fields')
     plus_node = ParseTreeNode('PLUS')
     plus_node.add_children(p[2].children)
     p[0].add_child(plus_node)
 
 def p_fields_plus_fieldlist(p):
     """fieldscmd : FIELDS PLUS fieldlist"""
-    p[0] = ParseTreeNode('FIELDS')
+    p[0] = ParseTreeNode('COMMAND', raw='fields')
     plus_node = ParseTreeNode('PLUS')
     plus_node.add_children(p[3].children)
     p[0].add_child(plus_node)
 
 def p_fields_minus_fieldlist(p):
     """fieldscmd : FIELDS MINUS fieldlist"""
-    p[0] = ParseTreeNode('FIELDS')
+    p[0] = ParseTreeNode('COMMAND', raw='fields')
     minus_node = ParseTreeNode('MINUS')
     minus_node.add_children(p[3].children)
     p[0].add_child(minus_node)
