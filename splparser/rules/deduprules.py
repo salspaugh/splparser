b6b20cda96527e5de27770baafef23fe9f44176c
Sara Alspaugh <saraalspaugh@gmail.com>
2013-07-22 23:06:30 -0700

    Updated dedup command.

--- splparser/rules/deduprules.py
+++ splparser/rules/deduprules.py
@@ -12,13 +12,15 @@ from splparser.lexers.deduplexer import tokens
 
 start = 'cmdexpr'
 
+boolean_options = ["consecutive", "keepempty", "keepevents"]
+
 def p_cmdexpr_dedup(p):
     """cmdexpr : dedupcmd"""
     p[0] = p[1]
 
 def p_dedup(p):
     """dedupcmd : DEDUP args"""
-    p[0] = ParseTreeNode('DEDUP')
+    p[0] = ParseTreeNode('COMMAND', raw='dedup')
     p[0].add_children(p[2])
 
 def p_dedup_args(p):
@@ -27,7 +29,11 @@ def p_dedup_args(p):
 
 def p_dedup_args_field(p):
     """args : INT fieldlist"""
-    p[0] = [ParseTreeNode('INT', raw=p[1], arg=True)] + p[2].children
+    eq = ParseTreeNode('EQ', raw='assign')
+    opt = ParseTreeNode('OPTION', raw='n')
+    int = ParseTreeNode('VALUE', type='INT', raw=p[1], is_argument=True)
+    eq.add_children([opt, int])
+    p[0] = [eq] + p[2].children
 
 def p_dedup_args_option(p):
     """args : fieldlist optionlist"""
@@ -39,11 +45,19 @@ def p_dedup_args_sort(p):
 
 def p_dedup_args_field_option(p):
     """args : INT fieldlist optionlist"""
-    p[0] = [ParseTreeNode('INT', raw=p[1], arg=True)] + p[2].children + p[3]
+    eq = ParseTreeNode('EQ', raw='assign')
+    opt = ParseTreeNode('OPTION', raw='n')
+    int = ParseTreeNode('VALUE', type='INT', raw=p[1], is_argument=True)
+    eq.add_children([opt, int])
+    p[0] = [eq] + p[2].children + p[3]
 
 def p_dedup_args_field_sort(p):
     """args : INT fieldlist sort"""
-    p[0] = [ParseTreeNode('INT', raw=p[1], arg=True)] + p[2].children + [p[3]]
+    eq = ParseTreeNode('EQ', raw='assign')
+    opt = ParseTreeNode('OPTION', raw='n')
+    int = ParseTreeNode('VALUE', type='INT', raw=p[1], is_argument=True)
+    eq.add_children([opt, int])
+    p[0] = [eq] + p[2].children + [p[3]]
 
 def p_dedup_args_option_sort(p):
     """args : fieldlist optionlist sort"""
@@ -51,35 +65,48 @@ def p_dedup_args_option_sort(p):
 
 def p_dedup_args_field_option_sort(p):
     """args : INT fieldlist optionlist sort"""
-    p[0] = [ParseTreeNode('INT', raw=p[1], arg=True)] + p[2].children + p[3] + [p[4]]
+    eq = ParseTreeNode('EQ', raw='assign')
+    opt = ParseTreeNode('OPTION', raw='n')
+    int = ParseTreeNode('VALUE', type='INT', raw=p[1], is_argument=True)
+    eq.add_children([opt, int])
+    p[0] = [eq] + p[2].children + p[3] + [p[4]]
 
 def p_optionlist(p):
     """optionlist : DEDUP_OPT EQ field optionlist"""
-    option = ParseTreeNode('EQ', raw='assign')
-    opt_node = ParseTreeNode('OPTION', raw=p[1])
-    opt_node.values.append(p[3])
-    option.add_children([opt_node, p[3]])
-    p[0] = [option] + p[4]
+    eq = ParseTreeNode('EQ', raw='assign')
+    opt = ParseTreeNode('OPTION', raw=p[1])
+    p[3].role = 'VALUE'
+    if opt.raw in boolean_options:
+        p[3].type = 'BOOLEAN'
+    opt.values.append(p[3])
+    eq.add_children([opt, p[3]])
+    p[0] = [eq] + p[4]
 
 def p_optionlist_comma(p):
     """optionlist : DEDUP_OPT EQ field COMMA optionlist"""
-    option = ParseTreeNode('EQ', raw='assign')
-    opt_node = ParseTreeNode('OPTION', raw=p[1])
-    opt_node.values.append(p[3])
-    option.add_children([opt_node, p[3]])
-    p[0] = [option] + p[5]
+    eq = ParseTreeNode('EQ', raw='assign')
+    opt = ParseTreeNode('OPTION', raw=p[1])
+    p[3].role = 'VALUE'
+    if opt.raw in boolean_options:
+        p[3].type = 'BOOLEAN'
+    opt.values.append(p[3])
+    eq.add_children([opt, p[3]])
+    p[0] = [eq] + p[5]
 
 def p_optionlist_one(p):
     """optionlist : DEDUP_OPT EQ field"""
-    option = ParseTreeNode('EQ', raw='assign')
-    opt_node = ParseTreeNode('OPTION', raw=p[1])
-    opt_node.values.append(p[3])
-    option.add_children([opt_node, p[3]])
-    p[0] = [option]
+    eq = ParseTreeNode('EQ', raw='assign')
+    opt = ParseTreeNode('OPTION', raw=p[1])
+    p[3].role = 'VALUE'
+    if opt.raw in boolean_options:
+        p[3].type = 'BOOLEAN'
+    opt.values.append(p[3])
+    eq.add_children([opt, p[3]])
+    p[0] = [eq]
 
 def p_sort(p):
     """sort : SORTBY sortfields"""
-    p[0] = ParseTreeNode('SORTBY')
+    p[0] = ParseTreeNode('FUNCTION', raw='sortby')
     p[0].add_children(p[2])
 
 def p_sortfields_list(p):
@@ -96,7 +123,7 @@ def p_sortfield_asc_func(p):
                  | sortfunc
                  | field
                  """
-    p[0] = ParseTreeNode('ASCENDING')
+    p[0] = ParseTreeNode('FUNCTION', raw='ascending')
     i = 1 if len(p)==2 else 2
     p[0].add_child(p[i])
 
@@ -104,12 +131,12 @@ def p_sortfield_desc_func(p):
     """sortfield : MINUS sortfunc
                  | MINUS field
                  """
-    p[0] = ParseTreeNode('DESCENDING')
+    p[0] = ParseTreeNode('FUNCTION', raw='descending')
     p[0].add_child(p[2])
 
 def p_sortfunc(p):
     """sortfunc : WORD LPAREN field RPAREN"""
-    p[0] = ParseTreeNode(p[1].upper())
+    p[0] = ParseTreeNode('FUNCTION', raw=p[1])
     p[0].add_child(p[3])
 
 def p_error(p):
