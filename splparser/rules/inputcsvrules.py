958c4cb8898c891d46285055fbe2f3195e5c6d28
Sara Alspaugh <saraalspaugh@gmail.com>
2013-07-22 17:24:01 -0700

    Updated inputcsv command.

--- splparser/rules/inputcsvrules.py
+++ splparser/rules/inputcsvrules.py
@@ -13,30 +13,33 @@ from splparser.lexers.inputcsvlexer import tokens
 
 start = 'cmdexpr'
 
+boolean_options = ["append", "events"]
+
 def p_cmdexpr_inputcsv(p):
     """cmdexpr : inputcsvcmd"""
     p[0] = p[1]
 
-def p_cmdexpr_inputcsv_single(p):
-    """inputcsvcmd : INPUTCSV"""
-    p[0] = ParseTreeNode('INPUTCSV')
-
 def p_cmdexpr_inputcsv_field(p):
     """inputcsvcmd : INPUTCSV wc_stringlist value"""
-    p[0] = ParseTreeNode('INPUTCSV')
+    p[0] = ParseTreeNode('COMMAND', raw='inputcsv')
     p[0].add_children(p[2].children)
+    p[3].role = 'FILENAME'
     p[0].add_child(p[3])
 
 def p_inputcsvcmd_inputcsv(p):
     """inputcsvcmd : INPUTCSV wc_stringlist
                    | INPUTCSV value"""
-    p[0] = ParseTreeNode('INPUTCSV')
+    p[0] = ParseTreeNode('COMMAND', raw='inputcsv')
     p[0].add_child(p[2])
+    if len(p[2].children) == 0:
+        p[2].role = 'FILENAME'
 
 def p_inputcsv_opt(p):
     """wc_string : INPUTCSV_OPT EQ value"""
     p[0] = ParseTreeNode('EQ', raw='assign')
     p[1] = ParseTreeNode('OPTION', raw=p[1])
+    if p[1].raw in boolean_options:
+        p[3].type = 'BOOLEAN'
     p[1].values.append(p[3])
     p[0].add_children([p[1], p[3]])
 
