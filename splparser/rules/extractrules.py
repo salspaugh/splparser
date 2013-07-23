a41578b9e2407bb3edeabe76b82885dbe36e7edd
Sara Alspaugh <saraalspaugh@gmail.com>
2013-07-22 17:02:58 -0700

    Updated extract command.

--- splparser/rules/extractrules.py
+++ splparser/rules/extractrules.py
@@ -6,13 +6,15 @@ import logging
 from splparser.parsetree import *
 from splparser.exceptions import SPLSyntaxError
 
-from splparser.rules.common.fieldlistrules import *
+from splparser.rules.common.fieldrules import *
 from splparser.rules.common.valuerules import *
 from splparser.rules.common.regexrules import *
 
 from splparser.lexers.extractlexer import tokens
 
 start = 'cmdexpr'
+
+boolean_options = ["auto", "clean_keys", "mv_add", "reload", "segment"]
 
 def p_cmdexpr_extractkv(p):
     """cmdexpr : extractkvcmd"""
@@ -21,21 +23,27 @@ def p_cmdexpr_extractkv(p):
 def p_cmdexpr_extractkv_single(p):
     """extractkvcmd : EXTRACT
                     | KV"""
-    p[0] = ParseTreeNode(p[1])
+    p[0] = ParseTreeNode('COMMAND', raw=p[1])
 
-def p_cmdexpr_extractkv_field(p):
-    """extractkvcmd : EXTRACT wc_stringlist fieldlist
-                    | KV wc_stringlist fieldlist"""
-    p[0] = ParseTreeNode(p[1])
+def p_cmdexpr_extractkv_both(p):
+    """extractkvcmd : EXTRACT wc_stringlist field
+                    | KV wc_stringlist field"""
+    p[0] = ParseTreeNode('COMMAND', raw=p[1])
     p[0].add_children(p[2].children)
-    p[0].add_children(p[3].children)
+    p[3].role = 'EXTRACTOR_NAME'
+    p[0].add_child(p[3])
 
-def p_extractkvcmd_extractkv(p):
+def p_extractkvcmd_extractkv_field(p):
+    """extractkvcmd : EXTRACT field
+                    | KV field"""
+    p[0] = ParseTreeNode('COMMAND', raw=p[1])
+    p[2].role = 'EXTRACTOR_NAME'
+    p[0].add_child(p[2])
+
+def p_extractkvcmd_extractkv_opts(p):
     """extractkvcmd : EXTRACT wc_stringlist
-                    | EXTRACT fieldlist
-                    | KV wc_stringlist
-                    | KV fieldlist"""
-    p[0] = ParseTreeNode(p[1])
+                    | KV wc_stringlist"""
+    p[0] = ParseTreeNode('COMMAND', raw=p[1])
     p[0].add_children(p[2].children)
 
 def p_extractkv_opt(p):
@@ -44,6 +52,8 @@ def p_extractkv_opt(p):
                  | EXTRACTKV_OPT EQ regex"""
     p[0] = ParseTreeNode('EQ', raw='assign')
     p[1] = ParseTreeNode('OPTION', raw=p[1])
+    if p[1].raw in boolean_options:
+        p[3].type = 'BOOLEAN'
     p[1].values.append(p[3])
     p[0].add_children([p[1],p[3]])
 
