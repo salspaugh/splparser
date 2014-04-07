92c3e4895c863299446d0d2746aa4a877276a547
Sara Alspaugh <saraalspaugh@gmail.com>
2014-04-06 19:53:53 -0700

    Fixed loadjob errors.

--- splparser/rules/loadjobrules.py
+++ splparser/rules/loadjobrules.py
@@ -18,18 +18,23 @@ def p_cmdexpr_loadjob(p):
     """cmdexpr : loadjobcmd"""
     p[0] = p[1]
 
-def p_cmdexpr_loadjob_field(p):
+def p_loadjob_value(p):
+    """loadjobcmd : LOADJOB value"""
+    p[0] = ParseTreeNode('COMMAND', raw='loadjob')
+    p[0].add_child(p[2])
+
+def p_loadjob_value_wcstringlist(p):
     """loadjobcmd : LOADJOB value wc_stringlist"""
     p[0] = ParseTreeNode('COMMAND', raw='loadjob')
     p[0].add_child(p[2])
     p[0].add_children(p[3].children)
 
-def p_loadjobcmd_loadjob(p):
+def p_loadjob_wcstringlist(p):
     """loadjobcmd : LOADJOB wc_stringlist"""
     p[0] = ParseTreeNode('COMMAND', raw='loadjob')
     p[0].add_children(p[2].children)
 
-def p_loadjob_opt(p):
+def p_wcstring(p):
     """wc_string : LOADJOB_OPT EQ value"""
     p[0] = ParseTreeNode('EQ', raw='assign')
     p[1] = ParseTreeNode('OPTION', raw=p[1])
@@ -38,12 +43,12 @@ def p_loadjob_opt(p):
     p[1].values.append(p[3])
     p[0].add_children([p[1], p[3]])
 
-def p_loadjob_opt_list(p):
+def p_wcstringlist(p):
     """wc_stringlist : wc_string"""
     p[0] = ParseTreeNode('EQ', raw='assign')
     p[0].add_child(p[1])
 
-def p_loadjob_optlist(p):
+def p_wcstring_wcstringlist(p):
     """wc_stringlist : wc_string wc_stringlist"""
     p[0] = ParseTreeNode('EQ', raw='assign')
     p[0].add_child(p[1])
